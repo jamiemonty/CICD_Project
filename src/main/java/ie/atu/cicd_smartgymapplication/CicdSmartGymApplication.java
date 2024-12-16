f496069e6c1c1c13651f0c900279cc3912c22e8a
Jamie <G00419525@atu.ie>
2024-12-16 22:41:13 +0000

    Created a new project for Josh Thompson & Jamie Montgomerys Smart Gym Companion App

---
+++ src/main/java/ie/atu/cicd_smartgymapplication/CicdSmartGymApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_smartgymapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSmartGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSmartGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicd_smartgymapplication/CicdSmartGymApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_smartgymapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSmartGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
